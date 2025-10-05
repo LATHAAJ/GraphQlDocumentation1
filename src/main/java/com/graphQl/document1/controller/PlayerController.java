2073bfa3ba6869f46e90b02c982733ae8ad820a4
LATHAAJ <122860428+LATHAAJ@users.noreply.github.com>
2025-10-05 18:06:39 +0530

    Revert "added schema changes"
    This reverts commit 2c0d4714047ef35541317e4f48e11c23573a5703.

--- src/main/java/com/graphQl/document1/controller/PlayerController.java
+++ src/main/java/com/graphQl/document1/controller/PlayerController.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 public class PlayerController {
   private final PlayerService playerService;
 
-  // Query Mappings
   @QueryMapping
   public List<Player> findAll() {
     return playerService.findAll();
@@ -33,80 +32,8 @@ public class PlayerController {
     return playerService.findByTeam(team);
   }
 
-  @QueryMapping
-  public Optional<Player> findByName(@Argument String name) {
-    return playerService.findByName(name);
-  }
-
-  @QueryMapping
-  public List<Player> findByPosition(@Argument String position) {
-    return playerService.findByPosition(position);
-  }
-
-  @QueryMapping
-  public List<Player> findActivePlayers() {
-    return playerService.findActivePlayers();
-  }
-
-  @QueryMapping
-  public List<Player> findPlayersByAgeRange(@Argument Integer minAge, @Argument Integer maxAge) {
-    return playerService.findPlayersByAgeRange(minAge, maxAge);
-  }
-
-  @QueryMapping
-  public List<Player> findPlayersBySalaryRange(@Argument Double minSalary, @Argument Double maxSalary) {
-    return playerService.findPlayersBySalaryRange(minSalary, maxSalary);
-  }
-
-  @QueryMapping
-  public Integer getPlayerCount() {
-    return playerService.getPlayerCount();
-  }
-
-  @QueryMapping
-  public Integer getTeamPlayerCount(@Argument Team team) {
-    return playerService.getTeamPlayerCount(team);
-  }
-
-  // Mutation Mappings
   @MutationMapping
-  public Player createPlayer(@Argument String name, @Argument Team team, @Argument String city, 
-                           @Argument Integer age, @Argument String position, 
-                           @Argument Integer jerseyNumber, @Argument Double salary) {
-    return playerService.createPlayer(name, team, city, age, position, jerseyNumber, salary);
-  }
-
-  @MutationMapping
-  public Optional<Player> updatePlayer(@Argument Integer id, @Argument String name, 
-                                      @Argument Team team, @Argument String city, 
-                                      @Argument Integer age, @Argument String position, 
-                                      @Argument Integer jerseyNumber, @Argument Double salary, 
-                                      @Argument Boolean isActive) {
-    return playerService.updatePlayer(id, name, team, city, age, position, jerseyNumber, salary, isActive);
-  }
-
-  @MutationMapping
-  public Boolean deletePlayer(@Argument Integer id) {
-    return playerService.deletePlayer(id);
-  }
-
-  @MutationMapping
-  public Optional<Player> transferPlayer(@Argument Integer id, @Argument Team newTeam) {
-    return playerService.transferPlayer(id, newTeam);
-  }
-
-  @MutationMapping
-  public Optional<Player> activatePlayer(@Argument Integer id) {
-    return playerService.activatePlayer(id);
-  }
-
-  @MutationMapping
-  public Optional<Player> deactivatePlayer(@Argument Integer id) {
-    return playerService.deactivatePlayer(id);
-  }
-
-  @MutationMapping
-  public Optional<Player> updatePlayerSalary(@Argument Integer id, @Argument Double newSalary) {
-    return playerService.updatePlayerSalary(id, newSalary);
+  public Player createPlayer(@Argument String name, @Argument Team team, @Argument String city) {
+    return playerService.createPlayer(name, team, city);
   }
 }
--- src/main/java/com/graphQl/document1/model/Player.java
+++ src/main/java/com/graphQl/document1/model/Player.java
@@ -1,4 +1,4 @@
 package com.graphQl.document1.model;
 
-public record Player(Integer id, String name, Team team, String city, Integer age, String position, Integer jerseyNumber, Double salary, Boolean isActive) {
+public record Player(Integer id, String name, Team team, String city) {
 }
